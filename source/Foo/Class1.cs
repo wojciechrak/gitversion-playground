38f041e813bc62d1ba4e5025f8d16b5196032eb8
Wojciech Rak <wojciech.rak@hotmail.com>
2017-10-06 20:21:37 +0200

    Change to .NET framework csproj

--- source/Foo/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Foo
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/Foo/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Foo
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
